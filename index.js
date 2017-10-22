4de97ae57607f22cd93300c4add4163e0dd91e3c
Harish Annam <harish.annam@gmail.com>
2017-10-22 15:38:59 +0000

    Fixed transaction sale parameters

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var gateway = braintree.connect({
-  accessToken: "access_token$production$2ccztk4gcbs8qv7w$261c4df4e64956dd8d0641a483e68f53"
+  accessToken: process.env.ACCESS_TOKEN
 });
 
 app.get("/client_token", function (req, res) {
   gateway.clientToken.generate({}, function (err, response) {
+    console.log("client token created");
     res.send(response.clientToken);
   });
 });
@@ -27,21 +28,26 @@ app.post("/checkout", function (req, res) {
       submitForSettlement: true
     }
   };
+  
+  console.log(saleRequest);
+  console.log("\n");
 
   gateway.transaction.sale(saleRequest, function (err, result) {
     if (err) {
       console.log(""+err);
       res.send("<h1>Error:  " + err + "</h1>");
     } else if (result.success) {
-      console.log(""+JSON.stringify(result));
+      console.log("SUCCESS : "+JSON.stringify(result));
       res.send("<h1>Success! Transaction ID: " + result.transaction.id + "</h1>");
     } else {
-      console.log(""+JSON.stringify(result));
+      console.log("ERROR : "+JSON.stringify(result));
       res.send("<h1>Error:  " + result.message + "</h1>");
     }
   });
 });
 
-app.listen(8000, function () {
+console.log("HARISH"+process.env.PORT);
+
+app.listen(process.env.PORT, function () {
   console.log('App listening on port 8000!');
 });
